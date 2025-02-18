e6c5540b1259a75f86de3bcd9761ef2378c5394e
ShouvikSarkar1 <shouviksarkarmusic@gmail.com>
2025-02-18 09:30:27 -0500

    homepage

--- miniproject2/src/routes/AppRoutes.jsx
+++ miniproject2/src/routes/AppRoutes.jsx
@@ -4,6 +4,7 @@ import HomePage from '../pages/HomePage'
 import SignUp from '../pages/SignUp'
 import Dashboard from '../pages/Dashboard'
 import MUIForm from '../components/LoginForm/MUIForm'
+import PageNotFound from '../pages/PageNotFound'
 
 const AppRoutes = (props) => {
   return (
@@ -11,6 +12,7 @@ const AppRoutes = (props) => {
         <Route index element ={<HomePage {...props}/>}/>
         <Route path='/signup' element={<SignUp {...props}/>}/>
         <Route path='/login' element={<MUIForm/>}/>
+        <Route path='/*' element={<PageNotFound {...props}/>}/>
         <Route path='/dash' element ={<Dashboard {...props}/>}/>
     </Routes>
   )
